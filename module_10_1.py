a14b54143f4b31e48f8cd32c979279aff7456294
AMacmep <cherkashinandrey@mail.ru>
2024-10-17 10:29:34 +0700

    17/10

---
+++ module_10_1.py
@@ -0,0 +1,38 @@
+# Домашнее задание по теме "Создание потоков".
+import time
+import threading
+from threading import Thread
+from datetime import datetime
+
+
+def write_words(word_count, file_name):
+    time_start = datetime.now()
+    work_file = open(file_name, 'w', encoding='utf-8')
+    for word_number in range(word_count):
+        work_file.write(f'Какое-то слово №{word_number + 1}\n')
+        time.sleep(0.1)
+    print(f'Завершилась запись в файл {file_name}')
+    time_finish = datetime.now()
+    fime_work_function = time_finish - time_start
+    print(fime_work_function)
+
+'''
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')'''
+
+thr_one = Thread(target=write_words, args=(10, 'example5.txt'))
+thr_two = Thread(target=write_words, args=(10, 'example6.txt'))
+thr_three = Thread(target=write_words, args=(10, 'example7.txt'))
+thr_four = Thread(target=write_words, args=(10, 'example8.txt'))
+
+thr_one.start()
+thr_two.start()
+thr_three.start()
+thr_four.start()
+
+thr_one.join()
+thr_two.join()
+thr_three.join()
+thr_four.join()
